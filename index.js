b682521bc47132d18672e13e998305f8102db1ef
Rajakanksha <119947167+Rajakanksha@users.noreply.github.com>
2024-06-29 20:46:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+function show_hide0() {
+    var click = document.getElementById("list-items");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ 
+ function show_hide1() {
+    var click = document.getElementById("h-list");
+    if(click.style.display ==="none") {
+       click.style.display ="inline-block";
+    } else {
+       click.style.display ="none";
+    } 
+    var click1 = document.getElementById("c-list");
+    if(click1.style.display ==="inline-block") {
+       click1.style.display ="none";
+    } 
+ }
+ function show_hide2() {
+    var click = document.getElementById("c-list");
+    if(click.style.display ==="none") {
+       click.style.display ="inline-block";
+    } else {
+       click.style.display ="none";
+    } 
+    var click1 = document.getElementById("h-list");
+    if(click1.style.display ==="inline-block") {
+       click1.style.display ="none";
+    } 
+ }
+ function show_deer() {
+    var click = document.getElementById("deer");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_elephant() {
+    var click = document.getElementById("elephant");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_girraffe() {
+    var click = document.getElementById("girraffe");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_monkey() {
+    var click = document.getElementById("monkey");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_lion() {
+    var click = document.getElementById("lion");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_tiger() {
+    var click = document.getElementById("tiger");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_cheeta() {
+    var click = document.getElementById("cheeta");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
+ function show_bear() {
+    var click = document.getElementById("bear");
+    if(click.style.display ==="none") {
+       click.style.display ="flex";
+    } else {
+       click.style.display ="none";
+    } 
+ }
